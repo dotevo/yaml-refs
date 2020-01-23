9c6d0806d35e12ae578a04829915cecc96d7847f
Adam Jordanek <dotevo@gmail.com>
2020-01-23 12:30:22 +0100

    chore: Formating

--- src/__tests__/1.test.ts
+++ src/__tests__/1.test.ts
@@ -4,87 +4,85 @@ import * as fs from 'fs';
 import * as YAML from 'yaml';
 
 describe('Reading', () => {
-  let doc: Document;
-  const b_json = JSON.parse('{"aaa": {"aaaa": "val"}}');
-  beforeEach(() => {
-    const str = fs.readFileSync('src/__tests__/res/1/test.yml', 'utf8');
-    doc = YAML.parseDocument(str, {'merge': true, customTags:[ref]});
-  });
+	let doc: Document;
+	const b_json = JSON.parse('{"aaa": {"aaaa": "val"}}');
+	beforeEach(() => {
+		const str = fs.readFileSync('src/__tests__/res/1/test.yml', 'utf8');
+		doc = YAML.parseDocument(str, { merge: true, customTags: [ref] });
+	});
 
-  test('1: Map via getIn fullpath', () => {
-    const i = doc.getIn(['a','aa']);
-    expect(i.toJSON()).toEqual(b_json);
-  });
+	test('1: Map via getIn fullpath', () => {
+		const i = doc.getIn(['a', 'aa']);
+		expect(i.toJSON()).toEqual(b_json);
+	});
 
-  test('1: Map via getIn Ref', () => {
-    const i = doc.getIn(['b']);
-    expect(i.toJSON()).toEqual(b_json);
-  });
+	test('1: Map via getIn Ref', () => {
+		const i = doc.getIn(['b']);
+		expect(i.toJSON()).toEqual(b_json);
+	});
 
-  test('2: Scalar via getIn fullpath', () => {
-    let j = doc.getIn(['a','aa','aaa', 'aaaa']);
-    expect(j).toEqual("val");
-  });
+	test('2: Scalar via getIn fullpath', () => {
+		let j = doc.getIn(['a', 'aa', 'aaa', 'aaaa']);
+		expect(j).toEqual('val');
+	});
 
-  test('2: Scalar via getIn in Ref', () => {
-    let j = doc.getIn(['b','aaa', 'aaaa']);
-    expect(j).toEqual("val");
-  });
+	test('2: Scalar via getIn in Ref', () => {
+		let j = doc.getIn(['b', 'aaa', 'aaaa']);
+		expect(j).toEqual('val');
+	});
 
-  test('3: Map via items fullpath', () => {
-    let j = doc.contents.items[0].value.items[0].value.items[0];
-    expect(j.toJSON()).toEqual(b_json);
-  });
+	test('3: Map via items fullpath', () => {
+		let j = doc.contents.items[0].value.items[0].value.items[0];
+		expect(j.toJSON()).toEqual(b_json);
+	});
 
-  test('3: Map via items Ref', () => {
-    let j = doc.contents.items[1].value.items[0];
-    expect(j.toJSON()).toEqual(b_json);
-  });
+	test('3: Map via items Ref', () => {
+		let j = doc.contents.items[1].value.items[0];
+		expect(j.toJSON()).toEqual(b_json);
+	});
 
-  test('4: Scalar via items fullpath', () => {
-    let j = doc.contents.items[0].value.items[0].value.items[0].value.items[0].value;
-    expect(j.toJSON()).toEqual("val");
-  });
+	test('4: Scalar via items fullpath', () => {
+		let j = doc.contents.items[0].value.items[0].value.items[0].value.items[0].value;
+		expect(j.toJSON()).toEqual('val');
+	});
 
-  test('4: Scalar via items Ref', () => {
-    let j = doc.contents.items[1].value.items[0].value.items[0].value;
-    expect(j.toJSON()).toEqual("val");
-  });
-
+	test('4: Scalar via items Ref', () => {
+		let j = doc.contents.items[1].value.items[0].value.items[0].value;
+		expect(j.toJSON()).toEqual('val');
+	});
 });
 
 describe('one file test (1st res)', () => {
-  let doc: Document;
-  let json: JSON;
-  let str: string;
-  beforeEach(() => {
-    str = fs.readFileSync('src/__tests__/res/1/test.yml', 'utf8');
-    doc = YAML.parseDocument(str, {'merge': true, customTags:[ref]});
-    json = JSON.parse(fs.readFileSync('src/__tests__/res/1/test.json', 'utf8'));
-  });
+	let doc: Document;
+	let json: JSON;
+	let str: string;
+	beforeEach(() => {
+		str = fs.readFileSync('src/__tests__/res/1/test.yml', 'utf8');
+		doc = YAML.parseDocument(str, { merge: true, customTags: [ref] });
+		json = JSON.parse(fs.readFileSync('src/__tests__/res/1/test.json', 'utf8'));
+	});
 
-  test('open', () => {
-    expect(doc.contents.toJSON()).toEqual(json);
-  });
+	test('open', () => {
+		expect(doc.contents.toJSON()).toEqual(json);
+	});
 
-  test('stringify', () => {
-    expect(String(doc)).toEqual(str);
-  });
+	test('stringify', () => {
+		expect(String(doc)).toEqual(str);
+	});
 
-  test('change in source', () => {
-    let j = doc.getIn(['a','aa']);
-    j.items[0].value = 'val2';
-    expect(j.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
-    const i = doc.getIn(['b']);
-    expect(i.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
-  });
+	test('change in source', () => {
+		let j = doc.getIn(['a', 'aa']);
+		j.items[0].value = 'val2';
+		expect(j.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
+		const i = doc.getIn(['b']);
+		expect(i.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
+	});
 
-  test('change in reference', () => {
-    let j = doc.getIn(['b']);
-    j.items[0].value = 'val2';
-    expect(j.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
-    const i = doc.getIn(['a','aa']);
-    expect(i.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
-  });
-
-)};
+	test('change in reference', () => {
+		let j = doc.getIn(['b']);
+		j.items[0].value = 'val2';
+		expect(j.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
+		const i = doc.getIn(['a', 'aa']);
+		expect(i.toJSON()).toEqual(JSON.parse('{"aaa": "val2"}'));
+	});
+});
--- src/loader.ts
+++ src/loader.ts
@@ -3,27 +3,27 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export default class MainDocument {
-  // FIXME: Remove any
-  private mDocuments: {[p: string]:Document|any;} = {};
-  private mOpts: any;
+	// FIXME: Remove any
+	private mDocuments: { [p: string]: Document | any } = {};
+	private mOpts: any;
 
-  constructor(opts: any) {
-    opts.loader = this;
-    this.mOpts = opts;
-  }
+	constructor(opts: any) {
+		opts.loader = this;
+		this.mOpts = opts;
+	}
 
-  // FIXME: Remove any
-  getDocument(p: string, context: Document|null|any = null): Document {
-    let file_path = path.resolve(p);
-    if (context != null) {
-      file_path = path.resolve(path.parse(context.options.path).dir, p);
-    }
-    // Load the document
-    if(this.mDocuments[file_path] == null) {
-      let opts = { ... this.mOpts };
-      opts.path = file_path;
-      this.mDocuments[file_path] = YAML.parseDocument(fs.readFileSync(file_path, 'utf8'),opts);
-    }
-    return this.mDocuments[file_path];
-  }
+	// FIXME: Remove any
+	getDocument(p: string, context: Document | null | any = null): Document {
+		let file_path = path.resolve(p);
+		if (context != null) {
+			file_path = path.resolve(path.parse(context.options.path).dir, p);
+		}
+		// Load the document
+		if (this.mDocuments[file_path] == null) {
+			let opts = { ...this.mOpts };
+			opts.path = file_path;
+			this.mDocuments[file_path] = YAML.parseDocument(fs.readFileSync(file_path, 'utf8'), opts);
+		}
+		return this.mDocuments[file_path];
+	}
 }
